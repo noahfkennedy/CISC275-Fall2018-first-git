b2c547e394edee482f5d83fbcf88dc3a186d2eb4
noahfkennedy <noahlk@udel.edu>
2018-09-11 16:34:54 -0400

    Adding java files to class

---
+++ Dog.java
@@ -0,0 +1,16 @@
+import java.util.*
+
+public class Dog {
+	public String name;
+	public int legs;
+
+	public Dog(String name, int legs) {
+		this.name = name;
+		this.legs = legs;
+	}
+
+	public String toString() { return this.name;}
+
+}
+
+
